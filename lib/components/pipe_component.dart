c947a12a431490860bcce802a9d9f53c017e9b22
JeejaFR <jeremy.defuentes@gmail.com>
2024-09-13 17:52:53 +0200

    feat: pipe design

--- lib/components/pipe_component.dart
+++ lib/components/pipe_component.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flappyanimal/provider/game_provider.dart';
 import 'package:flutter/material.dart';
 
 class Pipe extends StatelessWidget {
@@ -14,28 +17,46 @@ class Pipe extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const pipeImage = 'assets/pipe.png';
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Positioned(
       bottom: 0,
-      left: pipeX * MediaQuery.of(context).size.width,
+      left: pipeX * screenWidth,
       child: Column(
         children: [
-          Container(
-            width: MediaQuery.of(context).size.width * pipeWidth,
-            height: MediaQuery.of(context).size.height * (1 - pipeY - pipeGap),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 94, 94, 95),
-              border: Border.all(
-                  color: Colors.red, width: 2), // Bordure pour hitbox
+          Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.rotationX(pi),
+            // Partie supérieure du tuyau (qui vient du haut)
+            child: Container(
+              width: screenWidth * pipeWidth,
+              height: screenHeight * (1 - pipeY - pipeGap),
+              child: Image.asset(
+                pipeImage,
+                width: screenWidth * pipeWidth,
+                height: screenHeight * (1 - pipeY - pipeGap),
+                fit: BoxFit.fill,
+                alignment: Alignment
+                    .bottomCenter, // Aligne l'image vers le bas pour la continuité
+              ),
             ),
           ),
-          SizedBox(height: MediaQuery.of(context).size.height * pipeGap),
+          // Espace vide correspondant au "gap" entre les tuyaux
+          SizedBox(height: screenHeight * pipeGap),
+          // Partie inférieure du tuyau (qui vient du bas)
+          // Rotation à 180 degrés pour le tuyau du bas
           Container(
-            width: MediaQuery.of(context).size.width * pipeWidth,
-            height: MediaQuery.of(context).size.height * pipeY,
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 92, 90, 90),
-              border: Border.all(
-                  color: Colors.red, width: 2), // Bordure pour hitbox
+            width: screenWidth * pipeWidth,
+            height: screenHeight * pipeY,
+            child: Image.asset(
+              pipeImage,
+              width: screenWidth * pipeWidth,
+              height: screenHeight * pipeY,
+              fit: BoxFit.fill,
+              alignment: Alignment
+                  .topCenter, // Aligne l'image vers le haut pour la continuité
             ),
           ),
         ],
